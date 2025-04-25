87ea76e4dea9ea9df8fe4429d42b67efc84db328
Lacey Merryman <merrymanlacey@gmail.com>
2025-04-25 13:30:28 -1000

    modify initial

--- index.js
+++ index.js
@@ -9,10 +9,42 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = ([poem]) => {
+  const makeH2 = makeTag('h2')
+  const makeH3 = makeTag('h3')
+  const makeEm = makeTag('em')
+  const makeP = makeTag('p')
+
+  // Title and author section
+  const titleHTML = makeH2(poem.title)
+  const authorHTML = makeH3(makeEm(`by ${poem.author}`))
+
+  // Group lines into stanzas
+  const stanzas = poem.lines.reduce((acc, line) => {
+    if (line === '') {
+      acc.push([])
+    } else {
+      if (acc.length === 0) acc.push([])
+      acc[acc.length - 1].push(line)
+    }
+    return acc
+  }, [])
+
+  // Use pipe to turn lines into <p> with <br> between lines
+  const lineBreaks = lines =>
+    lines.map((line, i) =>
+      i === lines.length - 1 ? line : `${line}<br>`
+    ).join('')
+
+  const stanzaToParagraph = pipe(lineBreaks, makeP)
+  const stanzaHTML = stanzas.map(stanzaToParagraph).join('')
+
+  return titleHTML + authorHTML + stanzaHTML
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
   // renders the HTML string returned by makePoemHTML to #poem
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
 }
+
